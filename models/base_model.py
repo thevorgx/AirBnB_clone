e149b7005fa25e25529070801d749aa5fcffb070
thevorgx <kh.elallali@gmail.com>
2024-02-02 04:26:58 +0100

    task 4 __init__ method update

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,21 @@ from datetime import datetime
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """special method that initialize the public instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(
+                            self,
+                            key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """special method that return a formated string ready to be printed"""
