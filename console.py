7ec5f158bd5e840eea090ac40896deae291868c0
thevorgx <kh.elallali@gmail.com>
2024-02-05 02:36:04 +0100

    task 6 - console (interpreter for some basic commands)

--- console.py
+++ console.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""command prompt class(console)"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, command):
+        """quit when the user write the quit command"""
+        return (True)
+
+    def do_EOF(self, command):
+        """quit when user use ctrl+D"""
+        return (True)
+
+    def help_quit(self):
+        """print documentation for the "quit" command"""
+        print("Quit command to exit the program")
+
+    def emptyline(self):
+        """execute nothing when the user hit enter without command"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
