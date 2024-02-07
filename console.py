47fe54478dbf7446faeecb78a8e1c4177b567104
ercudu <ilyass.bentalha2@gmail.com>
2024-02-07 15:32:20 +0000

    add update task 7

--- console.py
+++ console.py
@@ -132,6 +132,45 @@ class HBNBCommand(cmd.Cmd):
 
         print(cls_instances)
 
+    def do_update(self, command):
+
+        classes = self.airbnb_classes
+        tokenize_cmd = command.split()
+
+        if not tokenize_cmd:
+            print("** class name missing **")
+            return
+
+        cls_name = tokenize_cmd[0]
+        if cls_name not in classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(tokenize_cmd) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = tokenize_cmd[1]
+        instance_to_upd = f"{cls_name}.{instance_id}"
+        all_instances = storage.all()
+        if instance_to_upd not in all_instances:
+            print("** attribute name missing **")
+            return
+
+        if len(tokenize_cmd) < 3:
+            print("** attribute name missing **")
+            return
+        attribute_name = tokenize_cmd[2]
+
+        if len(tokenize_cmd) < 4:
+            print("** value missing **")
+            return
+        else:
+            attribute_value = tokenize_cmd[3]
+            setattr(all_instances[instance_to_upd], attribute_name, attribute_value)
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
