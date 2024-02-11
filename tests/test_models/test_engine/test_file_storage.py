707495bb54c653c74938003f3ba63f1d92bf8993
thevorgx <kh.elallali@gmail.com>
2024-02-11 22:36:01 +0100

    test new method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,11 +4,13 @@
 
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 class File_Storage_Test(unittest.TestCase):
     """Base_Model_Test class (unittests for the base_model)"""
+
     def test_all_method(self):
-        """test the all method"""
+        """test for the all method"""
 
         storage = FileStorage()
         all_objects = storage.all()
@@ -16,6 +18,16 @@ class File_Storage_Test(unittest.TestCase):
         self.assertIsInstance(all_objects, dict)
         self.assertEqual(all_objects, storage._FileStorage__objects)
 
+    def test_new_method(self):
+        """test for the new method"""
+
+        storage = FileStorage()
+        obj = BaseModel()
+
+        object_key = f"{obj.__class__.__name__}.{obj.id}"
+        storage.new(obj)
+
+        self.assertIn(object_key, storage.all())
 
 if __name__ == "__main__":
     unittest.main()
