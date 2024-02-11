6ffbdd8db5da21276d9cff5e0f02693cef7e08a0
thevorgx <kh.elallali@gmail.com>
2024-02-11 20:05:31 +0100

    save method unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,14 +2,13 @@
 """unittest cases for base_model module"""
 
 
-
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 import uuid
 
 class Base_Model_Test(unittest.TestCase):
-
+    """Base_Model_Test class (unittests for the base_model)"""
 
     def test_no_arg_Initialization(self):
         """test if the initializations attributes are done correctly"""
@@ -33,10 +32,20 @@ class Base_Model_Test(unittest.TestCase):
         instance.my_number = 89
 
         my_str = instance.__str__()
-
         expected_output = "[BaseModel] (0ba07990-9b34-41c8-a9cf-fed53f7cd400) {'id': '0ba07990-9b34-41c8-a9cf-fed53f7cd400', 'created_at': datetime.datetime(2024, 2, 11, 16, 14, 47, 15113), 'updated_at': datetime.datetime(2024, 2, 11, 16, 14, 47, 15171), 'name': 'My First Model', 'my_number': 89}"
 
         self.assertEqual(my_str, expected_output)
+
+    def test_save_method(self):
+        """test the save method"""
+
+        instance = BaseModel()
+        original_updated_at = instance.updated_at
+
+        instance.save()
+        new_updated_at = instance.updated_at
+
+        self.assertNotEqual(new_updated_at, original_updated_at)
 
 
 if __name__ == "__main__":
